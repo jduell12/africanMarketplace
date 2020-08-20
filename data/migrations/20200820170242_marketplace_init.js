d195b4f7a3f7968d1450619e099cb5d765119eca
Jessica <duellje@oregonstate.edu>
2020-08-20 17:19:19 -0400

    added migration

---
+++ data/migrations/20200820170242_marketplace_init.js
@@ -0,0 +1,69 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("locations", (tbl) => {
+      tble.increments("id");
+      tbl.string("location", 256).notNullable().unique();
+    })
+    .createTable("owners", (tbl) => {
+      tbl.increments("id");
+      tbl
+        .integer("location_id")
+        .unsigned()
+        .references("locations.id")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+    })
+    .createTable("users", (tbl) => {
+      tbl.increments("id");
+      tbl.string("username", 256).notNullable().unique();
+      tbl.string("password", 256).notNullable();
+      tbl
+        .integer("owner_id")
+        .unsigned()
+        .references("owners.id")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+    })
+    .createTable("item_categories", (tbl) => {
+      tbl.increments("id");
+      tbl.string("category", 256).notNullable().unique();
+    })
+    .createTable("items", (tbl) => {
+      tbl.increments("id");
+      tbl.string("name", 256).notNullable().unique();
+      tbl
+        .integer("item_category_id")
+        .unsigned()
+        .references("item_categories.id")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+    })
+    .createTable("owner_items", (tbl) => {
+      tbl.increments(id);
+      tbl.integer("quantity").notNullable();
+      tbl.decimal("price").notNullable();
+      tbl.text("description");
+      tbl
+        .integer("owner_id")
+        .unsigned()
+        .references("owners.id")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+      tbl
+        .integer("item_id")
+        .unsigned()
+        .references("items.id")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropIfTableExists("owner_items")
+    .dropIfTableExists("items")
+    .dropIfTableExists("item_categories")
+    .dropIfTableExists("users")
+    .dropIfTableExists("owners")
+    .dropIfTableExists("locations");
+};

9d64b1634c4e80b56bd21d1ce38e95b55fdea4af
Jessica <duellje@oregonstate.edu>
2020-09-03 19:14:36 -0400

    successfully truncate tables in postgres

--- users/usersModel.test.js
+++ users/usersModel.test.js
@@ -1,10 +1,11 @@
 const db = require("../data/db-config");
 const Users = require("./usersModel");
+const knexfile = require("../knexfile");
 
 describe("UsersModel", () => {
   //wipes all tables in database clean so each test starts with empty tables
   beforeEach(async () => {
-    await db("users").truncate();
+    await db.raw("TRUNCATE users RESTART IDENTITY CASCADE");
   });
 
   describe("getUsers()", () => {
@@ -21,15 +22,26 @@ describe("UsersModel", () => {
   });
 
   describe("addUser(user)", () => {
-    it("adds a user", async () => {
+    it("adds a user successfully to an empty database", async () => {
       const user = {
         username: "dragon",
         password: "pass",
       };
+
+      const expected = [
+        {
+          username: "dragon",
+          password: "pass",
+          is_owner: false,
+          owner_id: null,
+          id: 1,
+        },
+      ];
       await Users.addUser(user);
 
       const users = await db("users");
       expect(users).toHaveLength(1);
+      expect(users).toEqual(expected);
     });
   });
 });

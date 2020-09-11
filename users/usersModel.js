75fca165f80fe9293f23da6a4517cea7df418554
Jessica <duellje@oregonstate.edu>
2020-09-11 15:52:03 -0400

    completed edit user tests

--- users/usersModel.js
+++ users/usersModel.js
@@ -3,12 +3,33 @@ const db = require("../data/db-config");
 module.exports = {
   getUsers,
   addUser,
+  editUser,
+  deleteUser,
+  getUserById,
 };
 
+//returns an array of users in the database
 function getUsers() {
   return db("users");
 }
 
+//returns number of records added
 async function addUser(user) {
   return db("users").insert(user, "id");
 }
+
+//updates the user with the given id and returns the count of records changed
+async function editUser(userId, user) {
+  return db("users")
+    .where({ id: userId })
+    .update(user)
+    .then((count) => {
+      return count;
+    });
+}
+
+async function deleteUser(user) {
+  return null;
+}
+
+async function getUserById(user) {}
--- users/usersModel.test.js
+++ users/usersModel.test.js
@@ -15,7 +15,11 @@ describe("UsersModel", () => {
     });
 
     it("gets array of users from db", async () => {
-      await db("users").insert({ username: "dragon", password: "pass" });
+      await db("users").insert({
+        username: "dragon",
+        password: "pass",
+        first_name: "Dragon",
+      });
       const users = await db("users");
       expect(users).toHaveLength(1);
     });
@@ -26,18 +30,21 @@ describe("UsersModel", () => {
       const user = {
         username: "dragon",
         password: "pass",
+        first_name: "Dragon",
       };
 
       const expected = [
         {
           username: "dragon",
           password: "pass",
+          first_name: "Dragon",
           is_owner: false,
           owner_id: null,
           id: 1,
         },
       ];
-      await Users.addUser(user);
+      count = await Users.addUser(user);
+      expect(count[0]).toEqual(1);
 
       const users = await db("users");
       expect(users).toHaveLength(1);
@@ -45,22 +52,34 @@ describe("UsersModel", () => {
     });
 
     it("adds a user successfully to a non-empty database", async () => {
+      await db("users").insert(
+        {
+          username: "wolf",
+          password: "pass",
+          first_name: "Wolf",
+        },
+        "id",
+      );
+
       const user = {
         username: "dragon",
         password: "pass",
+        first_name: "Dragon",
       };
 
       const expected = [
         {
-          username: "dragon",
+          username: "wolf",
           password: "pass",
+          first_name: "Wolf",
           is_owner: false,
           owner_id: null,
           id: 1,
         },
         {
-          username: "wolf",
+          username: "dragon",
           password: "pass",
+          first_name: "Dragon",
           is_owner: false,
           owner_id: null,
           id: 2,
@@ -68,14 +87,99 @@ describe("UsersModel", () => {
       ];
 
       await Users.addUser(user);
-      await Users.addUser({
-        username: "wolf",
-        password: "pass",
-      });
 
       const users = await db("users");
       expect(users).toHaveLength(2);
       expect(users).toEqual(expected);
+    });
+  });
+
+  describe("editUser(userId, user)", () => {
+    it("edits a user successfully when only 1 user in database", async () => {
+      await db("users").insert(
+        {
+          username: "dragon",
+          password: "pass",
+          first_name: "Dragon",
+        },
+        "id",
+      );
+
+      const expected = [
+        {
+          username: "dragon",
+          password: "pass",
+          first_name: "Jess",
+          is_owner: false,
+          owner_id: null,
+          id: 1,
+        },
+      ];
+
+      await Users.editUser(1, { first_name: "Jess" });
+      const users = await db("users");
+      expect(users).toHaveLength(1);
+      expect(users).toEqual(expected);
+    });
+
+    it("edits a user correctly when more than 1 user in database", async () => {
+      await db("users").insert(
+        {
+          username: "dragon",
+          password: "pass",
+          first_name: "Dragon",
+        },
+        "id",
+      );
+      await db("users").insert(
+        {
+          username: "wolf",
+          password: "pass",
+          first_name: "Wolf",
+        },
+        "id",
+      );
+      await db("users").insert(
+        {
+          username: "dragon2",
+          password: "pass",
+          first_name: "Jess",
+        },
+        "id",
+      );
+
+      const expected = [
+        {
+          username: "dragon",
+          password: "pass",
+          first_name: "Dragon",
+          is_owner: false,
+          owner_id: null,
+          id: 1,
+        },
+        {
+          username: "wolf",
+          password: "pass",
+          first_name: "Jeremy",
+          is_owner: false,
+          owner_id: null,
+          id: 2,
+        },
+        {
+          username: "dragon2",
+          password: "pass",
+          first_name: "Jess",
+          is_owner: false,
+          owner_id: null,
+          id: 3,
+        },
+      ];
+
+      await Users.editUser(2, { first_name: "Jeremy" });
+
+      const users = await db("users");
+      expect(users).toHaveLength(3);
+      expect(users).toEqual(expect.arrayContaining(expected));
     });
   });
 });
